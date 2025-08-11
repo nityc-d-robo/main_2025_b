f288ed5c41c8109e1c2e2bb398785363e7a4f422
ezomo <ezomo1daniel@gmail.com>
2025-08-11 17:32:05 +0900

    add motor_lib

--- src/functions/retaining_arm.rs
+++ src/functions/retaining_arm.rs
@@ -1,3 +1,4 @@
+use motor_lib::{md, Error, GrpcHandle, USBHandle};
 use safe_drive::{logger::Logger, pr_info};
 use std::sync::{Arc, RwLock};
 
@@ -17,12 +18,14 @@ impl RetainingArmState {
 
 // entryはArcでラップしたRwLockを受け取る設計にする
 pub fn entry(r_a_state: Arc<RwLock<RetainingArmState>>) {
+    let handle = GrpcHandle::new("http://192.168.0.206:50051");
     let _logger = Logger::new(&format!("{}/{}", NODE_NAME, FUNCTION_NAME));
     loop {
         // 参照のみ使いたいときはreadロックを取得
         let state = r_a_state.read().unwrap();
         if state.test == 1 {
             pr_info!(_logger, "on");
+            md::send_pwm(&handle, 0 as u8, 1000 as i16);
         }
     }
 }
